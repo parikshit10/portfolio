fdd3b45f27e3723cb6f3b52041662c4efdc27a13
B0211995 <b0211995@LTB0211995-MAC.local>
2020-09-20 13:07:01 +0530

    js added

---
+++ script.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+  $(".js-wp-1").waypoint(
+    function (direction) {
+      $(".js-wp-1").addClass("animate__animated animate__fadeInUpBig")
+    },
+    {
+      offset: "100%",
+    }
+  )
+
+  $(".js-wp-2").waypoint(
+    function (direction) {
+      $(".js-wp-2").addClass("animate__animated animate__fadeInUpBig")
+    },
+    {
+      offset: "100%",
+    }
+  )
+  $(".js-wp-3").waypoint(
+    function (direction) {
+      $(".js-wp-3").addClass("animate__animated animate__fadeInUpBig")
+    },
+    {
+      offset: "100%",
+    }
+  )
+  $(".js-wp-4").waypoint(
+    function (direction) {
+      $(".js-wp-4").addClass("animate__animated animate__fadeInUpBig")
+    },
+    {
+      offset: "100%",
+    }
+  )
+  $(".js-wp-5").waypoint(
+    function (direction) {
+      $(".js-wp-5").addClass("animate__animated animate__fadeInUpBig")
+    },
+    {
+      offset: "100%",
+    }
+  )
+  $(".js-wp-6").waypoint(
+    function (direction) {
+      $(".js-wp-6").addClass("animate__animated animate__fadeInUpBig")
+    },
+    {
+      offset: "100%",
+    }
+  )
+})
